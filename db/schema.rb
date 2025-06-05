997d9397e8292a4dc759124d37a0b2886f2f7e97
aidahenni96 <aidahenni96@icloud.com>
2025-06-05 08:48:08 +0200

    pseudo name last name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_01_162346) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_05_001120) do
   create_table "cart_products", force: :cascade do |t|
     t.integer "cart_id", null: false
     t.integer "product_id", null: false
@@ -71,6 +71,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_01_162346) do
     t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "pseudo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
