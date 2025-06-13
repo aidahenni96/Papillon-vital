90e5c0947d45bec6ef540277786612d4baa030fb
aidahenni96 <aidahenni96@icloud.com>
2025-06-13 14:21:41 +0200

    add

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -17,22 +17,3 @@ if ('serviceWorker' in navigator) {
 
 
 
-document.addEventListener('DOMContentLoaded', () => {
-  // Fonction pour cacher les alertes après 5 secondes (5000 ms)
-  setTimeout(() => {
-    const notice = document.getElementById('flash-notice');
-    const alert = document.getElementById('flash-alert');
-
-    if (notice) {
-      notice.style.transition = "opacity 0.5s ease";
-      notice.style.opacity = 0;
-      setTimeout(() => notice.remove(), 500); // Supprime le div après la transition
-    }
-
-    if (alert) {
-      alert.style.transition = "opacity 0.5s ease";
-      alert.style.opacity = 0;
-      setTimeout(() => alert.remove(), 500);
-    }
-  }, 5000); // 5 secondes avant de disparaître
-});
